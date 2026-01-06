5a632e811af4999b77f6d9f61ee653b428a0e7c5
Sanika Patil <sanikapatil2209@gmail.com>
2026-01-06 13:40:44 +0530

    cell status checked

--- wordlegame/src/components/Guess/Guess.jsx
+++ wordlegame/src/components/Guess/Guess.jsx
@@ -1,15 +1,54 @@
+// import React from 'react';
+// import { range } from '@/src/utils';
+// import {checkGuess} from '../../game-helpers';
+
+// function Cell({letter, status}) {
+//   const className = status ? `cell ${status}` : 'cell';
+//   return <div>
+//     <span className={className}>
+//       {letter}
+//         </span>
+//   </div>;
+// }
+// function Guess({value, answer}) {
+//   const result = checkGuess(value, answer);;
+//   return (
+//     <p className='guess'>
+//       {range(5).map((num) => (
+//         <Cell key={num}
+//         letter={result? result[num].letter : undefined}
+//         status={result? result[num].status : undefined} />
+//       ))}
+//     </p>
+//   );
+// }
+
+// export default Guess;
 import React from 'react';
 import { range } from '@/src/utils';
+import { checkGuess } from '../../game-helpers';
 
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+  return <span className={className}>{letter}</span>;
+}
 
-function Guess({value}) {
+function Guess({ value, answer }) {
+  const result = value ? checkGuess(value, answer) : null;
+
   return (
-    <p className='guess'>
-      {range(5).map((num) => (
-        <span key={num} className='cell'>
-          {value ? value[num] : ''}
-        </span>
-      ))}
+    <p className="guess">
+      {range(5).map((num) => {
+        const cell = result ? result[num] : null;
+
+        return (
+          <Cell
+            key={num}
+            letter={cell ? cell.letter : ''}
+            status={cell ? cell.status : ''}
+          />
+        );
+      })}
     </p>
   );
 }
--- wordlegame/src/components/GuessResult/GuessResult.jsx
+++ wordlegame/src/components/GuessResult/GuessResult.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Guess from '../Guess';
 import {NUM_OF_GUESSES_ALLOWED} from '../../constants';
 import { range } from '@/src/utils';
-function GuessResult({guesses=[]}) {
+function GuessResult({guesses=[], answer}) {
   return (
   <div>
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <Guess key={num} value={guesses[num]} />
+        <Guess key={num} value={guesses[num]} answer={answer} />
       ))}
     </div>
   </div>
@@ -17,19 +17,3 @@ function GuessResult({guesses=[]}) {
 export default GuessResult;
 
 
-// import React from 'react';
-// import Guess from '../Guess';
-// import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
-// import { range } from '@/src/utils';
-
-// function GuessResult({ guesses = [] }) {
-//   return (
-//     <div className="guess-results">
-//       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-//         <Guess key={num} value={guesses[num]} />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default GuessResult;
